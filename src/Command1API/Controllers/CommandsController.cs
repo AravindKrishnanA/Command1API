fb9c9a2d480367d0836b437f68c0f93a015819de
Aravind Krishnan A <krishnanaravind8702@gmail.com>
2021-06-26 14:16:45 +0530

    Db context initial setup

--- src/Command1API/Controllers/CommandsController.cs
+++ src/Command1API/Controllers/CommandsController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Command1API.Data;
+using Command1API.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Command1API.Controllers
@@ -10,10 +12,29 @@ namespace Command1API.Controllers
     [ApiController]
     public class CommandsController : ControllerBase
     {
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        private readonly ICommandAPIRepo _repository;
+        public CommandsController(ICommandAPIRepo repository)
         {
-            return new string[] { "this", "is", "hard", "coded" };
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        {
+            var commandItems = _repository.GetAllCommands();
+
+            return Ok(commandItems);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandById(int id)
+        {
+            var commandItem = _repository.GetCommandById(id);
+            if (commandItem == null)
+            {
+                return NotFound();
+            }
+            return Ok(commandItem);
         }
     }
 }
---
+++ src/Command1API/Data/ICommandAPIRepo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Command1API.Models;
+
+namespace Command1API.Data
+{
+    public interface ICommandAPIRepo
+    {
+        bool saveChanges();
+        IEnumerable<Command> GetAllCommands();
+        Command GetCommandById(int id);
+        void CreateCommand(Command cmd);
+        void UpdateCommand(Command cmd);
+        void DeleteCommand(Command cmd);
+    }
+}
