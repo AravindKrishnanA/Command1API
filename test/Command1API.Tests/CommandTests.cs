40f3ebca13d6461be1d605a7355022d86f56790b
Aravind Krishnan A <krishnanaravind8702@gmail.com>
2021-07-01 15:24:53 +0530

    CommandsController Intial commit

---
+++ test/Command1API.Tests/CommandTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Command1API.Models;
+
+namespace Command1API.Tests
+{
+    public class CommandTests : IDisposable
+    {
+        Command testCommand;
+        
+        public CommandTests()
+        {
+            testCommand = new Command
+            {
+                HowTo = "Do Something",
+                Platform = "Some platform",
+                CommandLine = "Some commandline"
+            };
+        }
+
+        public void Dispose()
+        {
+            testCommand = null;
+        }
+
+        [Fact]
+        public void CanChangeHowTo()
+        {
+            //Arrange 
+
+            //Act 
+            testCommand.HowTo = "Execute Unit Tests";
+
+            //Assert
+            Assert.Equal("Execute Unit Tests", testCommand.HowTo);
+        }
+
+        [Fact]
+
+        public void CanChangePlatform()
+        {
+            //Arrange
+
+            //Act
+            testCommand.Platform = "xUnit";
+
+            //Assert
+            Assert.Equal("xUnit", testCommand.Platform);
+        }
+
+        [Fact]
+
+        public void CanChangeCommandLine()
+        {
+            //Arrange
+
+            //Act
+            testCommand.CommandLine = "dotnet test";
+
+            //Assert
+            Assert.Equal("dotnet test", testCommand.CommandLine);
+        }
+
+    }
+}
---
+++ test/Command1API.Tests/CommandsControllerTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Command1API.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using AutoMapper;
+using Command1API.Models;
+using Command1API.Data;
+using Command1API.Profiles;
+
+namespace Command1API.Tests
+{
+    public class CommandsControllerTests
+    {
+        [Fact]
+        public void GetCommandItems_ReturnsZeroItems_WhenDBIsEmpty()
+        {
+            //Arrange
+            var mockRepo = new Mock<ICommandAPIRepo>();
+
+            mockRepo.Setup(repo => repo.GetAllCommands()).Returns(GetCommands(0));
+
+            var realProfile = new CommandsProfile();
+            var configuration = new MapperConfiguration(cfg => cfg.AddProfile(realProfile));
+            IMapper mapper = new Mapper(configuration);
+
+            var controller = new CommandsController(mockRepo.Object, mapper);
+        }
+
+        private List<Command> GetCommands(int num)
+        {
+            var commands = new List<Command>();
+            if (num > 0)
+            {
+                commands.Add(new Command
+                {
+                    Id = 0,
+                    HowTo = "How To generate a migration",
+                    CommandLine = "dotnet ef migrations add <Name of Migration>",
+                    Platform = ".NET Core EF"
+                });
+            }
+            return commands;
+        }
+    }
+}
